17d13da3a1abf9281874ba7ab5787a325153c2bf
Mark <69156806+UnityMark@users.noreply.github.com>
2023-04-27 13:02:59 +0500

    Add Solution

---
+++ CodeSolution/RemovingElements.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace Kata_8Kuy
+{
+    internal static class RemovingElements
+    {
+        
+        public static object[] RemoveEveryOther(object[] arr) // My Solution
+        {
+
+            int sizeOfArray = 0;
+
+            if ((double)arr.Length % 2 != 0)
+                sizeOfArray = (arr.Length / 2) + 1;
+
+            else
+                sizeOfArray = arr.Length / 2;
+
+
+            object[] objects = new object[sizeOfArray];
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    objects[count] = arr[i];
+                    count++;
+                }
+            }
+
+            return objects;
+
+        }
+
+        public static object[] RemoveEveryOther(object[] arr) // Best Solution 
+        {
+            return arr.Where((e, i) => i % 2 == 0).ToArray();
+        }
+        
+    }
+}
---
+++ TextSolution/Removing Elements.md
@@ -0,0 +1,47 @@
+# Removing Elements
+
+**My Solution**
+```C#
+public static class Kata
+    {
+        public static object[] RemoveEveryOther(object[] arr)
+        {
+
+            int sizeOfArray = 0;
+
+            if ((double)arr.Length % 2 != 0)
+                sizeOfArray = (arr.Length / 2) + 1;
+
+            else
+                sizeOfArray = arr.Length / 2;
+
+
+            object[] objects = new object[sizeOfArray];
+
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    objects[count] = arr[i];
+                    count++;
+                }
+            }
+          
+          return objects;
+
+        }
+    }
+```
+**Best Solution**
+```C#
+using System.Linq;
+
+public static class Kata
+{
+    public static object[] RemoveEveryOther(object[] arr)
+    {
+        return arr.Where((e, i) => i%2 == 0).ToArray();
+    }
+}
+```
